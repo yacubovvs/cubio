b2320a137a322ae158dc3ca2010ba850e390150c
yacubovvs <yacubovvs@yandex.ru>
2021-02-19 17:40:22 +0300

    fix

--- javaApp/src/ru/cubos/examples/arduino/ArduinoSerialExample.java
+++ javaApp/src/ru/cubos/examples/arduino/ArduinoSerialExample.java
@@ -19,8 +19,8 @@ public class ArduinoSerialExample {
                 //System.out.println("Counter interrupt. Pin: " + pin + ", number: " + counterNumber + ", time " + millis);
 
                 //double speed = 128.349 * TICKS_IN_ENCODER_COUNTER / (millis * 300.0);
-                double speed = 90.349 * TICKS_IN_ENCODER_COUNTER / (millis * 300.0);
-                millisDelay_test = (int)(8.0/100.0/speed*1000.0);
+                double speed = 128.349 * TICKS_IN_ENCODER_COUNTER / (millis * 600.0);
+                millisDelay_test = (int)(8.0/100.0/speed*1000.0) + 300;
                 System.out.printf("Angle speed " + speed + " mps\n");
                 System.out.printf("Rejecter " + millisDelay_test + " ms\n");
             }
@@ -29,17 +29,17 @@ public class ArduinoSerialExample {
 
             @Override
             public void onBoardStart(){
-                clearPinInterrupt(7);
+                //clearPinInterrupt(7);
                 //clearPinInterrupt(6);
                 //clearPinInterrupt(5);
                 //clearPinInterrupt(4);
 
                 pinMode(2, INPUT);
-                pinMode(7, INPUT);
                 pinMode(6, INPUT);
-                pinMode(4, INPUT);
+                pinMode(7, INPUT);
+                pinMode(8, INPUT);
 
-                pinMode(3, OUTPUT);
+                pinMode(4, OUTPUT);
                 //pinMode(6, INPUT);
                 //pinMode(5, INPUT);
                 //pinMode(4, INPUT);
@@ -48,10 +48,10 @@ public class ArduinoSerialExample {
 
                 counterModule.setCounter(2, TICKS_IN_ENCODER_COUNTER, 0);
 
-                setPinInterrupt(7);
+                //setPinInterrupt(2);
                 setPinInterrupt(6);
-                //setPinInterrupt(5);
-                setPinInterrupt(4);
+                setPinInterrupt(7);
+                setPinInterrupt(8);
 
                 /*
                 while(true){
@@ -61,21 +61,22 @@ public class ArduinoSerialExample {
                     digitalWrite(3, LOW);
                 }*/
 
-                digitalWrite(3, 1);
+                digitalWrite(4, 1);
             }
 
             @Override
             public void digitalInterruptReply(int pin, int value, long time){
                 System.out.println("OnInterrupt: pin: " + pin + ", value: " + value + ", time: " + time);
 
-                if(pin==4 && value == 1){
+
+                if(pin==7 && value == 1){
                     (new Thread(new Runnable() {
                         @Override
                         public void run() {
                             delay(millisDelay_test);
-                            digitalWrite(3, 0);
+                            digitalWrite(4, 0);
                             delay(250);
-                            digitalWrite(3, 1);
+                            digitalWrite(4, 1);
                         }
                     })).start();
                 }
@@ -83,8 +84,8 @@ public class ArduinoSerialExample {
 
         };
 
-        arduinoSerialConnector.setPort("COM24");
-        //arduinoSerialConnector.setPort("/dev/cu.usbserial-1410");
+        //arduinoSerialConnector.setPort("COM24");
+        arduinoSerialConnector.setPort("/dev/cu.usbserial-1410");
         counterModule.setConnector(arduinoSerialConnector);
         arduinoSerialConnector.addModule(counterModule);
         arduinoSerialConnector.connect();
